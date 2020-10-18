e8574c642fe273cb5d18f03b4fc9f5e88948c37f
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-10-19 03:23:14 +0800

    Route::get('hello/{name}', function($name) {     return 'Hello,'.$name; }); 瀏覽 http://localhost:8000/hello/tom 能看到 Hello,tom字

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//印出welcome
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function() {
+    return 'welcome';
+});//
+*/
+
+Route::get('hello/{name}', function($name) {
+    return 'Hello,'.$name;
+});
+
+
+/*Route::get('r1', function() {
+    return redirect('r2');
+});
+Route::get('r2', function() {
+    return view('welcome');
+});
+
+
+
