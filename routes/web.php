5ef6ebdbc35eb023d66ad405b8b761a0e0fa2fe5
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-10-21 10:58:26 +0800

    練習7-1 route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/',[\App\Http\Controllers\HomeController::class,'index'])->name('home.index');
+
 //印出welcome
 /*
 Route::get('/', function () {
